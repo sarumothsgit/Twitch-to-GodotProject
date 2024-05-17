b82594943eafb163c9d2172f6828ef5c9dbb3e41
Sarah <quiet@Sarahs-MacBook-Air.local>
2024-05-17 04:40:42 -0700

    Add .DS_Store to .gitignore, remove wsserverbotscript.py, add twitchbotwsserver.py

--- twitchbotwsserver.py
+++ twitchbotwsserver.py
@@ -1,45 +1,34 @@
 import os
 import twitchio
 from twitchio.ext import commands
-import asyncio
 import websockets
+import asyncio
 
 access_token = os.getenv("TWITCH_ACCESS_TOKEN")
-channel = "#eskimoths"
+channel = os.getenv("TWITCH_CHANNEL")
 
 class ServerBot(commands.Bot):
     def __init__(self):
         print("!def __init__(self):!")
-        print("\n")
-
         super().__init__(token=access_token, prefix='!', initial_channels=[channel])
         self.ws_payload_queue = asyncio.Queue()
 
     async def websocketserver(self, ws, path):
         print("!async def websocketserver(self ws, path):!")
-        print("\n")
-
         while True:
+            print("ws_payload sent to wsclientscript.gd.")
             ws_payload = await self.ws_payload_queue.get()
             await ws.send(ws_payload)
-            print("ws_payload sent to wsclientscript.gd.")
-            print("\n")
 
     async def event_ready(self):
         print("!async def event_ready(self):!")
-        print("\n")
-
-        self.ws_server = await websockets.serve(self.websocketserver, "localhost", 5000)
+        self.ws_server = await websockets.serve(self.websocketserver, "localhost", 1116)
 
     async def event_message(self, message):
         print("!async def event_message(self, message):!")
-        print("\n")
-
         if message.echo:
             return
-
         await self.handle_commands(message)
-
         raw_ws_payload = f"NAME[{message.author.name}]CONTENT[{message.content}]"
         print(f"raw_ws_payload: NAME[{message.author.name}]CONTENT[{message.content}]")
         await self.ws_payload_queue.put(raw_ws_payload)
